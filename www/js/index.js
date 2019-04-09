d520ac95fb7b4ccbe9e2657110bdf220210debbd
MadhityaGM23 <48473745+MadhityaGM23@users.noreply.github.com>
2019-04-09 14:49:41 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=QaHf2dkA30mTwAU2XFm39wS4MYWXzC45cFiluCGY";
         
         $("#loadImage").click(function(){
             $.ajax({
